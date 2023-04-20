9db34bc491ae4f9d464a4c7b980057925c8507f9
issaalmusawi <issa.al.musawi@gmail.com>
2023-04-20 21:58:49 +0000

    decrypt funksjon

--- tcp_client.go
+++ tcp_client.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.2:8080")
+	conn, err := net.Dial("tcp", "172.17.0.4:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,22 +16,41 @@ func main() {
 	//nytt
 
 	message :=[]rune(os.Args[1])
-	kryptertMelding, err := mycrypt.Krypter(message, 4)
+	encryptedMessage, err := mycrypt.Krypter(message, 4)
 	if err != nil{
 		log.Fatal(err)
 	}
 
-	log.Println("Kryptert melding: ", string(kryptertMelding))
+	log.Println("Kryptert melding: ", string(message))
 
- 	_, err = conn.Write([]byte(string(kryptertMelding)))
+ 	_, err = conn.Write([]byte(string(encryptedMessage)))
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
+	
 	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil{
+		log.Println(err)
+		return
+	}
+
+	encryptedResponse :=[]rune(string(buf[:n]))
+	decryptedResponse, err := mycrypt.Krypter(encryptedResponse, -4)
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	log.Println("reply from proxy: ", string(decryptedResponse))
+
+
+
+/*	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	} 
 	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	log.Printf("reply from proxy: %s", response)*/
 }
